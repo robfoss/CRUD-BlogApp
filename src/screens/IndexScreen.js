ca75cad6609b23717c33a92e46cc2ce9a219b729
robfoss <robfoss@rocketmail.com>
2021-03-31 10:40:09 -0400

    edit functionality

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,60 @@
+import React, { useContext } from 'react'
+import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity } from 'react-native';
+import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
+
+
+
+const IndexScreen = ({ navigation }) => {
+    const { state, addBlogPost, deleteBlogPost } = useContext(Context)
+    return (
+        <View>
+            <FlatList
+                data={state}
+                keyExtractor={(blogPost) => blogPost.title}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+                            <View style={styles.row}>
+                                <Text style={styles.title}>{item.title}</Text>
+                                <TouchableOpacity
+                                    onPress={() => deleteBlogPost(item.id)}>
+                                    <Feather style={styles.icon} name="trash" />
+                                </TouchableOpacity>
+                            </View>
+                        </TouchableOpacity>
+                    )
+                }}
+            />
+        </View>
+    )
+};
+
+IndexScreen.navigationOptions = ({ navigation }) => {
+    return {
+        headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+                <Feather name="plus" size={30} />
+            </TouchableOpacity>
+        ),
+    };
+}
+
+export default IndexScreen
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        borderTopWidth: 1,
+        borderColor: 'gray',
+        paddingHorizontal: 10,
+    },
+    title: {
+        fontSize: 18,
+    },
+    icon: {
+        fontSize: 24
+    }
+})
